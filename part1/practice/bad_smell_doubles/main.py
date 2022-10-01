e8d827c7eb9449f39a891e6e944a9d095fcd5505
denextden <extenden98@gmail.com>
2022-10-01 15:50:59 +0300

    .

--- part1/practice/bad_smell_doubles/main.py
+++ part1/practice/bad_smell_doubles/main.py
@@ -5,17 +5,19 @@
 # Напишите свой метод `sorted_func`, 
 # учитывая особенности всех представленных методов
 
-
 class SomeClass:
     def __init__(self):
         self.lst = [3, 2, 1, 4, 2, 1]
 
-    def sorted(self):
-        self.lst.sort()
-        return self.lst
-
-    def sorting(self):
-        return sorted(self.lst)
-
-    def asc_sorting(self):
+    def sorted_func(self):
         return sorted(self.lst, reverse=False)
+
+
+if __name__ == "__main__":
+    some_inst = SomeClass()
+    print(some_inst.sorted_func())
+
+
+
+test = SomeClass()
+print(test.sorted_func())
